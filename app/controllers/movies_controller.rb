73cc2bddc7cd7fbea5cd5166e3a48bc9551a65ff
SupansaTan <s6201012620236@email.kmutnb.ac.th>
2021-08-22 12:54:18 +0700

    edit variable

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
     def index
-        @movies = Movie.all
-        @sort_movies = @movies.sort_by(&:title)
+        movies = Movie.all
+        @sort_movies = movies.sort_by(&:title)
     end
 
     def show
