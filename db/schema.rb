f34a4a518cc497045b8ebb276af31758b81d1706
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-09-20 04:26:08 +0700

    fix table moviegoers already exist in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_115933) do
+ActiveRecord::Schema.define(version: 2021_09_19_185853) do
 
   create_table "moviegoers", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2021_09_19_115933) do
     t.datetime "release_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "potatoes"
+    t.text "comments"
+    t.integer "moviegoer_id"
+    t.integer "movie_id"
   end
 
 end
