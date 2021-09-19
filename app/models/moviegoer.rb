d1c45d174bbd0b5a374cfb503c39ea9d2d508a59
natthamon55 <natthamon2012@hotmail.com>
2021-09-20 02:14:16 +0700

    add model review

--- app/models/moviegoer.rb
+++ app/models/moviegoer.rb
@@ -1,10 +1,4 @@
 class Moviegoer < ApplicationRecord
-  attr_accessor :uid, :provider, :name # see text for explanation
-  has_many :reviews
-  def self.create_with_omniauth(auth)
-    Moviegoer.create!(
-      :provider => auth["provider"],
-      :uid => auth["uid"],
-      :name => auth["info"]["name"])
-  end
+    has_many :reviews
+
 end
