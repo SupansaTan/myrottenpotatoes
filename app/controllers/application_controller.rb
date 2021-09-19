e0c0d047ec8f7706cc8a307002be756ac0065151
natthamon55 <natthamon2012@hotmail.com>
2021-09-19 20:01:54 +0700

    set current user in app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action :set_config
+
+    protect_from_forgery with: :exception
+    before_action : set_current_user, :authenticate!, :set_config
+
+    def set_current_user
+        @current_user = Moviegoer.find_by(id: session[:user_id])
+    end
 
     require 'themoviedb'
     Tmdb::Api.key("56a532b46b2f4d48499071df7a75663e")
