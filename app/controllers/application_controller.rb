dda5d7275b9306a04aa4894f9812c8cd01261c9f
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-09-19 21:32:48 +0700

    fix conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,13 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-    before_action :set_config
-    before_action :set_current_user
-    protected # prevents method from being invoked by a route
-=======
 
     protect_from_forgery with: :exception
-    before_action : set_current_user, :authenticate!, :set_config
+    before_action   :set_current_user, :authenticate!, :set_config
 
     def set_current_user
         @current_user = Moviegoer.find_by(id: session[:user_id])
     end
 
-    protect_from_forgery with: :exception
-    before_action : set_current_user, :authenticate!, :set_config
-
-    def set_current_user
-        @current_user = Moviegoer.find_by(id: session[:user_id])
-    end
->>>>>>> main
-
+    # config TMDB
     require 'themoviedb'
     Tmdb::Api.key("56a532b46b2f4d48499071df7a75663e")
 
@@ -28,32 +16,11 @@ class ApplicationController < ActionController::Base
     end
 
     protected
-  
+
     def authenticate!
-        
         unless @current_user
-            redirect_to login_path
+            #redirect_to login_path
         end
     end
-
-
-
-<<<<<<< HEAD
-    def set_current_user
-        @current_user ||= Moviegoer.find_by_id(session[:user_id])
-      end
 end
 
-=======
-    protected
-  
-    def authenticate!
-        
-        unless @current_user
-            redirect_to login_path
-        end
-    end
-
-
-end
->>>>>>> main
