941a708f1be27cfdfe8ea47639dd2fdb865431d5
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-09-20 04:00:39 +0700

    add search movie in TMDb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :movies
   root :to => redirect('/movies')
+
+  # tmdb
+  post '/movies/search_tmdb'
+
+  # authentication
   get  'auth/:provider/callback' => 'sessions#create'
   post 'logout' => 'sessions#destroy'
   get  'auth/failure' => 'sessions#failure'
