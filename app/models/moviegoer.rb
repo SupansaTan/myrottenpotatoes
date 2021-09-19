1a6d5b6e2512d828759dd54d0f82d1c8572907a2
pprim <hathaipat44@hotmail.co.th>
2021-09-20 02:14:21 +0700

    update moviegoer and add login bt

--- app/models/moviegoer.rb
+++ app/models/moviegoer.rb
@@ -1,6 +1,9 @@
 class Moviegoer < ApplicationRecord
+
   attr_accessor :uid, :provider, :name # see text for explanation
   has_many :reviews
+  has_many :movies, :through => :reviews
+
   def self.create_with_omniauth(auth)
     Moviegoer.create!(
       :provider => auth["provider"],
