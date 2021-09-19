f8843fa640f8434b17a5dd60cb554be93748d0a5
pprim <hathaipat44@hotmail.co.th>
2021-09-20 02:14:21 +0700

    update moviegoer and add login bt

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :movies
   root :to => redirect('/movies')
-  get  'auth/:provider/callback' => 'sessions#create'
+  get  'auth/twitter' => 'sessions#create'
+  post  'auth/:provider/callback' => 'sessions#create'
   post 'logout' => 'sessions#destroy'
   get  'auth/failure' => 'sessions#failure'
 end
