631c95ab78890888db3ec64abd84e1564cd338d7
pprim <hathaipat44@hotmail.co.th>
2021-09-21 23:57:11 +0700

    integrate Devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_185853) do
-
-  create_table "moviegoers", force: :cascade do |t|
-    t.string "name"
-    t.string "provider"
-    t.string "uid"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_09_21_162313) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -34,6 +26,18 @@ ActiveRecord::Schema.define(version: 2021_09_19_185853) do
     t.text "comments"
     t.integer "moviegoer_id"
     t.integer "movie_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
