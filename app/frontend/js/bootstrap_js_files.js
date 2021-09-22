b181cc8fa2e4258119e4c259a9f94566216754c2
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-09-22 16:43:45 +0700

    change navbar style on application layout

--- app/frontend/js/bootstrap_js_files.js
+++ app/frontend/js/bootstrap_js_files.js
@@ -1,3 +1,1 @@
-import 'bootstrap/js/src/dropdown'
-import 'bootstrap/js/src/alert'
-import 'bootstrap/js/src/button'
+import 'bootstrap'
---
+++ config/webpack/custom.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack')
+
+module.exports = {
+    resolve: {
+        alias: {
+            $: 'jquery/src/jquery',
+            jQuery: 'jquery/src/jquery',
+            jquery: 'jquery',
+            'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ],
+    // Eliminate CORS issue
+    devServer: {
+        host: 'localhost',
+        port: 3000
+    }
+}
