8be979cd1b908ce49dd541cf10ab1576633f8d72
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-09-23 21:09:39 +0700

    add method for create movie from tmdb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   root :to => redirect('/movies')
 
   # tmdb
-  post '/movies/search_tmdb'
+  post '/movies/search_tmdb' => 'movies#search_tmdb'
+  post '/movies/create_from_tmdb' => 'movies#create_from_tmdb'
 
   # authentication
   get  'auth/twitter' => 'sessions#create'
