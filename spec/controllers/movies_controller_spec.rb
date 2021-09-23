0307e53fadafb336a89b8683700e7c65d446b4c3
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-09-23 17:57:39 +0700

    edit movie spec for test searching tmdb

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -5,19 +5,23 @@ describe MoviesController do
     before :each do
       @fake_results = [double('movie1'), double('movie2')]
     end
+
     it 'calls the model method that performs TMDb search' do
-      expect(Movie).to receive(:find).with('hardware').
+      expect(Movie).to receive(:find_in_tmdb).with('hardware').
         and_return(@fake_results)
-      post :search_tmdb, params: {:search_key => 'hardware'}
+      post :search_tmdb, params: {:search_terms => 'hardware'}
     end
+
     describe 'after valid search' do
       before :each do
-        allow(Movie).to receive(:find).and_return(@fake_results)
-        post :search_tmdb, params: {:search_key => 'hardware'}
+        allow(Movie).to receive(:find_in_tmdb).and_return(@fake_results)
+        post :search_tmdb, params: {:search_terms => 'hardware'}
       end
+
       it 'selects the Search Results template for rendering' do
-        expect(response).to render_template('search_tmdb')
+        expect(response).to render_template('tmdb')
       end
+
       it 'makes the TMDb search results available to that template' do
         expect(assigns(:movies))==(@fake_results)
       end
