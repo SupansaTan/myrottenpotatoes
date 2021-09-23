18a7d399c2b94b1f217d9fc8ee8e3b5c0302c5b7
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-09-23 17:57:39 +0700

    edit movie spec for test searching tmdb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -34,8 +34,12 @@ class Movie < ActiveRecord::Base
   def self.find_in_tmdb(string)
     begin
       Tmdb::Movie.find(string)
-    rescue Tmdb::InvalidApiKeyError
-      raise Movie::InvalidKeyError, 'Invalid API key'
+    rescue NoMethodError => tmdb_gem_exception
+      if Tmdb::Api.response['code'] == '401'
+        raise Movie::InvalidKeyError, 'Invalid API key'
+      else
+        raise tmdb_gem_exception
+      end
     end
   end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,12 +3,20 @@ require 'rails_helper'
 describe Movie do
   describe 'searching Tmdb by keyword' do
     context 'with valid API key' do
-      # elided for brevity
+      it 'should call Tmdb with title keywords' do
+        expect(Tmdb::Movie).to receive(:find).with('Inception')
+        Movie.find_in_tmdb('Inception')
+      end
     end
+
     context 'with invalid API key' do
-      it 'raises an InvalidKeyError' do
-        allow(Tmdb::Movie).to receive(:find).and_raise(Tmdb::InvalidApiKeyError)
-        expect { Movie.find('Inception') }.
+      before :each do
+        allow(Tmdb::Movie).to receive(:find).and_raise(NoMethodError)
+        allow(Tmdb::Api).to receive(:response).and_return({'code' => '401'})
+      end
+
+      it 'should raise an InvalidKeyError with no API key' do
+        expect { Movie.find_in_tmdb('Inception') }.
           to raise_error(Movie::InvalidKeyError)
       end
     end
