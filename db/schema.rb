c1222ab226eaeb255cdb0b68fc8b38c796acfd25
pprim <hathaipat44@hotmail.co.th>
2021-09-24 12:29:38 +0700

    set current_user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_170311) do
+ActiveRecord::Schema.define(version: 2021_09_24_032225) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
