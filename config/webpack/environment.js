e42d16eab3f5edcd913bfa95cbe86e53a2ae6197
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-09-26 19:14:32 +0700

    fix dependency

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
-
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
+        jQuery: 'jquery/src/jquery',
+        Popper: ['popper.js', 'default']
     })
 )
+
+module.exports = environment
