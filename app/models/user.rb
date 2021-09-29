b372b3ed0fc9d7f3583d9a5b39503c560366777c
pprim <hathaipat44@hotmail.co.th>
2021-09-30 01:49:35 +0700

    fix login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
-  attr_accessor :uid, :provider, :name # see text for explanation
+  attr_accessor :uid, :provider, :name
   has_many :reviews
   has_many :movies, :through => :reviews
 
@@ -13,6 +13,9 @@ class User < ApplicationRecord
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
+        user.provider = data["provider"] if user.provider.blank?
+        user.uid = data["uid"] if user.uid.blank?
+        user.name = data["name"] if user.name.blank?
       end
     end
   end
