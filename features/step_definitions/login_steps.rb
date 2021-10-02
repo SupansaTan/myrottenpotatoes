ab7ef6b0a1e1ab4089aa7ace422ff595a86f9be5
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-10-03 02:38:57 +0700

    update log in and movie story

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -2,24 +2,28 @@
 #Config Success case:
 Before('@omniauth_test_success') do
     OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:facebook] = { 
-        :provider  => "facebook",
-        :uid       => '1234567',
-        :info      => {
-            :email      => "testtest@email.com",
-            :first_name => "John",
-            :last_name  => "Doe",
-            :name       => "John Doe"
-            # any other attributes you want to stub out for testing
+    Capybara.default_host = 'http://localhost:3000'
+  
+    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+      :provider  => "facebook",
+      :uid       => '1234567',
+      :info      => {
+        :email      => "testtest@email.com",
+        :first_name => "John",
+        :last_name  => "Doe",
+        :name       => "John Doe"
       }
-    }
+    })
 end
-
-#Config Failure Case
+  
+#Config Failure case:
 Before('@omniauth_test_failure') do
     OmniAuth.config.test_mode = true
-    [:default, :facebook].each do |service|
+        [:default, :facebook].each do |service|
         OmniAuth.config.mock_auth[service] = :invalid_credentials
-        # or whatever status code you want to stub
     end
+end
+
+After('@omniauth_test_failure') do
+    OmniAuth.config.test_mode = false
 end
