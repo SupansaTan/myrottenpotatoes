785adb35236af568b5c26b01497024ddc28180be
PannawatKingkaew <s6201012620163@email.kmutnb.ac.th>
2021-10-03 11:33:38 +0700

    fix application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,7 +22,7 @@ window.$ = window.JQuery = JQuery
 import "bootstrap"
 
 import "@fortawesome/fontawesome-free/css/all" // font awesome
-
+import "@fortawesome/fontawesome-free/js/all"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
